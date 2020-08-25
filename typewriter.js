3e2e6a5175ffabf0bf7367b9f5942fec0c0bf000
Junsung Park <zeipar@gmail.com>
2020-08-25 12:28:04 -0700

    Update typewriter.js
    Change the order where time counter is added, to accurately execute the first iteration of the loop at time=0, and add newline character in the end. Also changed variable name 'timer' to 'delay.

--- typewriter.js
+++ typewriter.js
@@ -3,16 +3,17 @@
 const sentence = "hello there from lighthouse labs";
 
 const superTimer = () => {
-  let time = 0;
+  let delay = 0;
   for (const char of sentence) {
-    time += 50;
     setTimeout(() => {
       process.stdout.write(char);
-    }, time) // <= 1s delay to make it noticeable. Can dial it down later.
+    }, delay) // <= 1s delay to make it noticeable. Can dial it down later.
   }
+  delay += 50;
+  
   setTimeout(() => {
     process.stdout.write('\n');
-  }, time)
+  }, delay)
 }
 
 superTimer();
